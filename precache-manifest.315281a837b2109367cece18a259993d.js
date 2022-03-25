103d8e494e7e4ce29e502701759f29f127f98da0
William Grube <williamgrube53@gmail.com>
2022-03-25 14:13:40 -0500

    Updates

---
+++ precache-manifest.315281a837b2109367cece18a259993d.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ab18e258dee96106b52",
+    "url": "/Library/static/css/main.52629065.chunk.css"
+  },
+  {
+    "revision": "7ab18e258dee96106b52",
+    "url": "/Library/static/js/main.7ab18e25.chunk.js"
+  },
+  {
+    "revision": "4bb05d8fed1e4d6e8559",
+    "url": "/Library/static/js/1.4bb05d8f.chunk.js"
+  },
+  {
+    "revision": "ce68a6b3cb96ef4cc87d",
+    "url": "/Library/static/js/2.ce68a6b3.chunk.js"
+  },
+  {
+    "revision": "2e85b34c57ca51749c68",
+    "url": "/Library/static/js/runtime~main.2e85b34c.js"
+  },
+  {
+    "revision": "e7a5e1e0b6d35f5d64a46aa3955df5f7",
+    "url": "/Library/static/media/Library.e7a5e1e0.svg"
+  },
+  {
+    "revision": "bb4a1a2bd361b535645749afeb500e86",
+    "url": "/Library/static/media/Undraw_Books.bb4a1a2b.svg"
+  },
+  {
+    "revision": "c11647cafa4f63cfd2a182ee371a2dd7",
+    "url": "/Library/static/media/empty_cart.c11647ca.svg"
+  },
+  {
+    "revision": "7e9421250a1f6b9c011e076f152f1942",
+    "url": "/Library/static/media/wave.7e942125.svg"
+  },
+  {
+    "revision": "98dc96304e5c82f1c8a8503d684c946c",
+    "url": "/Library/index.html"
+  }
+];
